4850d2cf8cbf4eebcdd48b836b18b0b9060b6171
Hristina Mateeva <99602294+HristinaMateeva@users.noreply.github.com>
2022-05-08 00:12:58 +0300

    Update 03_even_powers_of_2.py

--- lectures/04_lecture_for_loops/03_even_powers_of_2.py
+++ lectures/04_lecture_for_loops/03_even_powers_of_2.py
@@ -2,5 +2,5 @@
 n = int(input())
 
 for power in range(0, n + 1, 2):
-    # print(math.pow(2, power)) - Функция от math за повдигане на степен
+    # print(math.pow(2, power))
     print(2 ** power)
