daa9fcee7531f84b4faa4360990e3ee034fbe730
Hristina Mateeva <99602294+HristinaMateeva@users.noreply.github.com>
2022-05-09 20:35:27 +0300

    Add files via upload

---
+++ exercises/01_exercise_first_steps_in_coding/09_fish_tank.py
@@ -0,0 +1,10 @@
+length_cm = int(input())
+width_cm = int(input())
+height_cm = int(input())
+capacity_percent = float(input())
+empty_volume_tank_cm = length_cm * width_cm * height_cm
+# 1 liter is equal to 1 dm^3
+empty_volume_tank_liters = empty_volume_tank_cm * 0.001  # Може да се напише като: volume_tank_cm / 1000
+capacity_water = capacity_percent / 100
+needed_water = empty_volume_tank_liters * (1 - capacity_water)
+print(needed_water)
