91dd08ad8939a508d6fcbd383779648c8737ae6f
Hristina Mateeva <99602294+HristinaMateeva@users.noreply.github.com>
2022-05-13 17:15:31 +0300

    Update 02_half_sum_element.py

--- exercises/04_exercise_for_loops/02_half_sum_element.py
+++ exercises/04_exercise_for_loops/02_half_sum_element.py
@@ -1,14 +1,13 @@
 count_of_numbers = int(input())
 sum_of_all_elements = 0
 max_number = " "
-for number in range(count_of_numbers):  #range(1, count_of_numbers + 1)
+for number in range(count_of_numbers):  
     current_number = int(input())
     if number == 0:
         max_number = current_number
     sum_of_all_elements += current_number
     if current_number > max_number:
         max_number = current_number
-#max_number == sum_of_all_elements / 2 - ни дава число float
 if max_number == sum_of_all_elements - max_number:
     print("Yes")
     print(f"Sum = {max_number}")
