833679cd812dc4f874fa8132163216e292964dac
Hristina Mateeva <hmateeva@gmail.com>
2022-05-27 20:59:51 +0300

    Update 06_the_most_powerful_word.py

--- 01_programming_basics/former_exams/04_exam_06-07_july_2019/06_the_most_powerful_word.py
+++ 01_programming_basics/former_exams/04_exam_06-07_july_2019/06_the_most_powerful_word.py
@@ -5,7 +5,6 @@ command = input()
 current_most_powerful_word = " "
 max_points = 0
 points_current_word = 0
-type_calculation = " "
 
 while command != "End of words":
     sum_current_word = 0
@@ -17,13 +16,9 @@ while command != "End of words":
         if word[0] == chr(65) or word[0] == chr(69) or word[0] == chr(73) or word[0] == chr(79) or word[0] == chr(85) or \
                 word[0] == chr(89) or word[0] == chr(97) or word[0] == chr(101) or word[0] == chr(105) or word[
             0] == chr(111) or word[0] == chr(117) or word[0] == chr(121):
-            type_calculation = "variant1"
+            points_current_word = sum_current_word * length_word
         else:
-            type_calculation = "variant2"
-    if type_calculation == "variant1":
-        points_current_word = sum_current_word * length_word
-    else:
-        points_current_word = math.floor(sum_current_word / length_word)
+            points_current_word = math.floor(sum_current_word / length_word)
     if points_current_word > max_points:
         max_points = points_current_word
         current_most_powerful_word = word
