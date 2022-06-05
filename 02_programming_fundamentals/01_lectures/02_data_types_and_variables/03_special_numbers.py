2eca84ff5d3abb724fbfb277fdf79126090cd842
Hristina Mateeva <hmateeva@gmail.com>
2022-06-05 22:08:54 +0300

    Create 03_special_numbers.py

---
+++ 02_programming_fundamentals/01_lectures/02_data_types_and_variables/03_special_numbers.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+for num in range(1, number + 1):
+    result = 0
+    num_as_str = str(num)
+    for char in num_as_str:
+        result += int(char)
+    if result == 5 or result == 7 or result == 11:
+        print(f"{num} -> True")
+    else:
+        print(f"{num} -> False")
