cf00c68035c3c5f82002ddb95334f22cab844db0
Hristina Mateeva <hmateeva@gmail.com>
2022-06-09 18:37:13 +0300

    Create 02_find_the_capitals_3.py

---
+++ 02_programming_fundamentals/03_more_exercises/01_basic_syntax_conditional_statements_and_loops/02_find_the_capitals_3.py
@@ -0,0 +1,11 @@
+CAPITAL_LETTERS_RANGE = range(ord("A"),ord("["))
+
+input_string = input()
+
+result_list = []
+
+for index in range (len(input_string)):
+    if input_string[index].isupper():
+        result_list.append(index)
+
+print(result_list)
