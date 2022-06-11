5bc9d2a86af772cdd96362cbaf4e90087c260db7
Hristina Mateeva <hmateeva@gmail.com>
2022-06-11 13:43:30 +0300

    Create 01_integer_operations.py

---
+++ 02_programming_fundamentals/02_exercises/02_data_types_and_variables/01_integer_operations.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+forth_number = int(input())
+
+operation_1 = (first_number + second_number)
+operation_2 = int(operation_1 / third_number)
+result = operation_2 * forth_number
+
+print(result)
