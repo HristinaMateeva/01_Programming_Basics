3e2c0d590b5b7812998df1cd55a8ec72a7d78eb7
Hristina Mateeva <hmateeva@gmail.com>
2022-06-13 20:22:43 +0300

    Create 06_triples_of_latin_letters.py

---
+++ 02_programming_fundamentals/02_exercises/02_data_types_and_variables/06_triples_of_latin_letters.py
@@ -0,0 +1,7 @@
+number = int(input())
+
+for first_char in range(number):
+    for second_char in range(number):
+        for third_char in range(number):
+            print(f"{chr(97 + first_char)}{chr(97 + second_char)}{chr(97 + third_char)}")
+            
