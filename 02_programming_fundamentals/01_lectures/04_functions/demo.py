11ad8142606de4acc1ac54c4d19bc897f750f258
Hristina Mateeva <hmateeva@gmail.com>
2022-07-02 10:09:18 +0300

    Create demo.py

---
+++ 02_programming_fundamentals/01_lectures/04_functions/demo.py
@@ -0,0 +1,16 @@
+# def custom_split(text, sep):
+#     result = text.split(sep)
+#     return result
+#
+# print(custom_split("Hello;there", ";"))
+# print(custom_split("How are you", " "))
+# print(custom_split("blue,green,red", ","))
+
+def multiply_five_self(num):
+    res = num * num
+    return res
+
+
+number = int(input())
+
+multiply_five_self(number)
