86b4d8fb49d66293ac238980a51ac00120224b95
Hristina Mateeva <hmateeva@gmail.com>
2022-07-23 18:40:18 +0300

    Create 04_number_classification_4.py

---
+++ 02_programming_fundamentals/02_exercises/05_list_advanced/04_number_classification_4.py
@@ -0,0 +1,13 @@
+numbers_list = input().split(", ")
+
+number_list_int = [int(num) for num in numbers_list]
+
+positive_numbers = [str(num) for num in number_list_int if num >= 0]
+negative_numbers = [str(num) for num in number_list_int if num < 0]
+even_numbers = [str(num) for num in number_list_int if num % 2 == 0]
+odd_numbers = [str(num) for num in number_list_int if num % 2 == 1]
+
+print(f"Positive: {', '.join(positive_numbers)}")
+print(f"Negative: {', '.join(negative_numbers)}")
+print(f"Even: {', '.join(even_numbers)}")
+print(f"Odd: {', '.join(odd_numbers)}")
