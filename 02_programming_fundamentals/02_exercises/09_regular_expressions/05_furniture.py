ba93d163d5113b2fce13aa953eefac930cf54e33
Hristina Mateeva <hmateeva@gmail.com>
2022-08-30 20:08:46 +0300

    Create 05_furniture.py

---
+++ 02_programming_fundamentals/02_exercises/09_regular_expressions/05_furniture.py
@@ -0,0 +1,20 @@
+import re
+
+pattern = r">>([a-zA-Z]+)<<(\d+\.?\d*)\!(\d+)"
+total_cost = 0
+bought_furniture = []
+command = input()
+
+while not command == "Purchase":
+    match = re.search(pattern, command)
+    if match:
+        furniture, price, quantity = match.groups()
+        bought_furniture.append(furniture)
+        total_cost += int(quantity) * float(price)
+
+    command = input()
+
+print("Bought furniture:")
+for furniture in bought_furniture:
+    print(furniture)
+print(f"Total money spend: {total_cost:.2f}")
