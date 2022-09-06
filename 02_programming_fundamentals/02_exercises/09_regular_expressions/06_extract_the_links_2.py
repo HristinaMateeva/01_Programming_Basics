b40d691e6b84a3045bcdb52aaa8a901dd9947287
Hristina Mateeva <hmateeva@gmail.com>
2022-09-06 18:29:11 +0300

    Create 06_extract_the_links_2.py

---
+++ 02_programming_fundamentals/02_exercises/09_regular_expressions/06_extract_the_links_2.py
@@ -0,0 +1,10 @@
+import re
+
+data = input()
+# pattern = r"(^|(?<=\s))w{3}\.[A-Za-z0-9-]+(\.[a-z]+)+($|(?=\s))" - the zero test 2 is incorrect
+pattern = r"(^|(?<=\s))w{3}\.[A-Za-z0-9-]+(\.[a-z]+)+"
+
+while data:
+    for el in re.finditer(pattern, data):
+        print(el.group())
+    data = input()
