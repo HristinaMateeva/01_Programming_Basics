de157e5208f073bcab8f672e6bf165827696a1eb
Hristina Mateeva <hmateeva@gmail.com>
2022-10-23 21:24:59 +0300

    Create 01_sum_matrix_elements_2.py

---
+++ 03_python_advanced/01_lectures/03_multidimensional_lists/01_sum_matrix_elements_2.py
@@ -0,0 +1,15 @@
+rows, columns = [int(el) for el in input().split(", ")]
+
+matrix = []
+result = 0
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split(", ")]
+    matrix.append(nums)
+
+for row_index in range(len(matrix)):
+    for col_index in range(len(matrix[row_index])):
+        result += matrix[row_index][col_index]
+
+print(result)
+print(matrix)
