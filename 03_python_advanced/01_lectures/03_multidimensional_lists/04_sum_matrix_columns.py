8320b52679e82486bf86d2ed221f9a68b1ba10da
Hristina Mateeva <hmateeva@gmail.com>
2022-10-24 22:51:30 +0300

    Create 04_sum_matrix_columns.py

---
+++ 03_python_advanced/01_lectures/03_multidimensional_lists/04_sum_matrix_columns.py
@@ -0,0 +1,13 @@
+rows, columns = [int(el) for el in input().split(", ")]
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split()]
+    matrix.append(nums)
+
+for col_index in range(columns):
+    result = 0
+    for row_index in range(rows):
+        result += matrix[row_index][col_index]
+    print(result)
