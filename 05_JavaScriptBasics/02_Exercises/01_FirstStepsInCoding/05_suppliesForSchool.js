8d8f8575d8dc48a5232f029a094813eb8072f91b
Hristina Mateeva <hmateeva@gmail.com>
2022-11-26 10:54:12 +0200

    Create 05_suppliesForSchool.js

---
+++ 05_JavaScriptBasics/02_Exercises/01_FirstStepsInCoding/05_suppliesForSchool.js
@@ -0,0 +1,17 @@
+function supplies(input){
+    let packPensPrice = 5.80
+    let packMarkerPrice = 7.20
+    let detergentPricePerLiter = 1.20
+
+    let packsPens = Number(input[0])
+    let packsMarkers = Number(input[1])
+    let litersDeetergent = Number(input[2])
+    let percentDiscount = Number(input[3]) / 100
+
+    let result = packPensPrice * packsPens + packMarkerPrice * packsMarkers + detergentPricePerLiter * litersDeetergent
+    result =  result * (1 - percentDiscount)
+    console.log(result)
+}
+
+supplies(["2 ", "3 ", "4 ", "25 "])
+supplies(["4 ", "2 ", "5 ", "13 "])
