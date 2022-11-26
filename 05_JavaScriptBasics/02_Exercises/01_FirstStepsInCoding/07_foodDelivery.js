d508b773196b10f973c7dc804d18c4eecde12ac9
Hristina Mateeva <hmateeva@gmail.com>
2022-11-26 10:54:17 +0200

    Create 07_foodDelivery.js

---
+++ 05_JavaScriptBasics/02_Exercises/01_FirstStepsInCoding/07_foodDelivery.js
@@ -0,0 +1,20 @@
+function foodDelivery(input){
+    let chickenMenues = Number(input[0])
+    let fishMenues = Number(input[1])
+    let vegeterianMenues = Number(input[2])
+
+    let chickenPrice = 10.35
+    let fishPrice = 12.40
+    let vegeterianPrice = 8.15
+    let delivery = 2.50
+    let dessertPercent = 0.20
+
+    let priceMenues = chickenMenues * chickenPrice + fishMenues * fishPrice + vegeterianMenues * vegeterianPrice
+    let dessertPrice = priceMenues * dessertPercent
+    let totalSum = priceMenues + dessertPrice + delivery
+
+    console.log(totalSum)
+}
+
+foodDelivery(["2 ", "4 ", "3 "])
+foodDelivery(["9 ", "2 ", "6 "])
