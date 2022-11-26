1555dd1d4a01c731da678fcc93c68c1104ed10ad
Hristina Mateeva <hmateeva@gmail.com>
2022-11-26 10:54:06 +0200

    Create 03_depositCalculator.js

---
+++ 05_JavaScriptBasics/02_Exercises/01_FirstStepsInCoding/03_depositCalculator.js
@@ -0,0 +1,10 @@
+function calculator(input){
+    let deposit = Number(input[0])
+    let period = Number(input[1])
+    let annual_percent = Number(input[2]) / 100
+    let result = deposit + period * ((deposit * annual_percent) / 12)
+    console.log(result)
+}
+
+calculator(["200 ", "3 ", "5.7 "])
+calculator(["2350", "6 ", "7 "])
