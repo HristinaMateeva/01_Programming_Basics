892b8c39ec2d55419f6ce70ce48c5c3b40b4f81b
Hristina Mateeva <hmateeva@gmail.com>
2022-11-30 22:03:24 +0200

    Create 04_vegetableMarket.js

---
+++ 05_JavaScriptBasics/03_MoreExercises/01_FirstStepsInCoding/04_vegetableMarket.js
@@ -0,0 +1,14 @@
+function vegetableMarket(input){
+    let priceVegetablePerKg = Number(input[0])
+    let priceFruitPerKg = Number(input[1])
+    let kilogramsVegetables = Number(input[2])
+    let kilogramsFruits = Number(input[3])
+
+    let totalSum = priceVegetablePerKg * kilogramsVegetables + priceFruitPerKg * kilogramsFruits
+    let totalSumEuro = totalSum / 1.94
+
+    console.log(totalSumEuro.toFixed(2))
+}
+
+vegetableMarket(["0.194", "19.4", "10", "10"])
+vegetableMarket(["1.5", "2.5", "10", "10"])
