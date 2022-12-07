403c403deafbfca16b47f63f55003010613b2fe1
Hristina Mateeva <hmateeva@gmail.com>
2022-12-07 20:37:51 +0200

    Create 04_toyShop.js

---
+++ 05_JavaScriptBasics/02_Exercises/02_ConditionalStatements/04_toyShop.js
@@ -0,0 +1,32 @@
+function toyShop(input){
+    let puzzlePrice = 2.60
+    let talkingDollPrice = 3
+    let teddyBearPrice = 4.10
+    let minionPrice = 8.20
+    let truckPrice = 2
+
+    let tripPrice = Number(input[0])
+    let numPuzzles = Number(input[1])
+    let numTalkingDolls = Number(input[2])
+    let numTeddyBears = Number(input[3])
+    let numMinions = Number(input[4])
+    let numTrucks = Number(input[5])
+
+    let numToys = numPuzzles + numTalkingDolls + numTeddyBears + numMinions + numTrucks
+    let totalPrice = numPuzzles * puzzlePrice + numTalkingDolls * talkingDollPrice + numTeddyBears * teddyBearPrice + numMinions * minionPrice + numTrucks * truckPrice
+
+    if (numToys >= 50){
+        totalPrice *= 0.75
+    }
+
+    totalPrice *= 0.90
+
+    if (totalPrice >= tripPrice){
+        console.log(`Yes! ${(totalPrice - tripPrice).toFixed(2)} lv left.`)
+    } else {
+        console.log(`Not enough money! ${(tripPrice - totalPrice).toFixed(2)} lv needed.`)
+    }
+}
+
+toyShop(["40.8", "20", "25", "30", "50", "10"])
+toyShop(["320", "8", "2", "5", "5", "1"])
