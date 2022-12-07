58597dd3f3e58c6eb6419339fae4e181580b59d6
Hristina Mateeva <hmateeva@gmail.com>
2022-12-07 20:37:53 +0200

    Create 05_godzillaVsKong.js

---
+++ 05_JavaScriptBasics/02_Exercises/02_ConditionalStatements/05_godzillaVsKong.js
@@ -0,0 +1,25 @@
+function gozillaVsKong(input){
+    let budget = Number(input[0])
+    let numStatists = Number(input[1])
+    let priceStatistEquipment = Number(input[2])
+    let priceDecor = budget * 0.10
+    
+    if (numStatists > 150){
+        priceStatistEquipment *= 0.90
+    }
+
+    let totalPrice = (numStatists * priceStatistEquipment) + priceDecor
+    let difference = Math.abs(budget - totalPrice)
+
+    if (totalPrice > budget){
+        console.log("Not enough money!")
+        console.log(`Wingard needs ${difference.toFixed(2)} leva more.`)
+    } else {
+        console.log("Action!" )
+        console.log(`Wingard starts filming with ${difference.toFixed(2)} leva left.`)
+    }
+}
+
+gozillaVsKong(["20000", "120", "55.5"])
+gozillaVsKong(["15437.62", "186", "57.99"])
+gozillaVsKong(["9587.88", "222", "55.68"])
