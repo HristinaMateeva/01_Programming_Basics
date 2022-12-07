9c61489438ca6a57c0d1a1d3c5eb2d54fc9c564e
Hristina Mateeva <hmateeva@gmail.com>
2022-12-07 20:37:46 +0200

    Create 02_bonusScore.js

---
+++ 05_JavaScriptBasics/02_Exercises/02_ConditionalStatements/02_bonusScore.js
@@ -0,0 +1,29 @@
+function bonusScore(input){
+    let initialPoints = Number(input[0])
+    let bonusPoints = 0
+    let result = 0
+
+    if (initialPoints <= 100){
+        bonusPoints = 5
+    } else if (initialPoints <= 1000){
+        bonusPoints = initialPoints * 0.2
+    } else {
+        bonusPoints = initialPoints * 0.1
+    }
+
+
+    if (initialPoints % 2 == 0){
+        bonusPoints += 1
+    } else if (initialPoints % 10 == 5){
+        bonusPoints += 2
+    }
+    
+    result = initialPoints +  bonusPoints
+    console.log(bonusPoints)
+    console.log(result)
+}
+
+bonusScore(["20"])
+bonusScore(["175"])
+bonusScore(["2703"])
+bonusScore(["15875"])
