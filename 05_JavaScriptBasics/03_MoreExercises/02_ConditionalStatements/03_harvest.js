faafe6d54c4cf99f79865d80ee74eafe84fe9453
Hristina Mateeva <hmateeva@gmail.com>
2022-12-10 20:23:19 +0200

    Create 03_harvest.js

---
+++ 05_JavaScriptBasics/03_MoreExercises/02_ConditionalStatements/03_harvest.js
@@ -0,0 +1,22 @@
+function harvest(input){
+    let vineyardArea = Number(input[0])
+    let grapesPerSquareMeter = Number(input[1])
+    let neededLitersWine = Number(input[2])
+    let numWorkers = Number(input[3])
+
+    let totalQuantityGrapes = vineyardArea * grapesPerSquareMeter
+    let litersWine = totalQuantityGrapes * 0.40 / 2.5
+
+    if (litersWine >= neededLitersWine){
+        let leftWine = Math.ceil(litersWine - neededLitersWine)
+        let winePerWorker = Math.ceil(leftWine / numWorkers)
+        console.log(`Good harvest this year! Total wine: ${Math.floor(litersWine)} liters.`)
+        console.log(`${leftWine} liters left -> ${winePerWorker} liters per person.`)
+    } else {
+        let difference = Math.floor(neededLitersWine - litersWine)
+        console.log(`It will be a tough winter! More ${difference} liters wine needed.`)
+    }
+}
+
+harvest(["650", "2", "175", "3"])
+harvest(["1020" , "1.5", "425", "4"])
