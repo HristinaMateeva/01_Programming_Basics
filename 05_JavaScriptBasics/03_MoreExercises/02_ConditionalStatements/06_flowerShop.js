69fe96b9d6bee16e94b1cab5422cacd220628dc5
Hristina Mateeva <hmateeva@gmail.com>
2022-12-10 20:23:27 +0200

    Create 06_flowerShop.js

---
+++ 05_JavaScriptBasics/03_MoreExercises/02_ConditionalStatements/06_flowerShop.js
@@ -0,0 +1,24 @@
+function flowerShop(input){
+    let magnolii = Number(input[0])
+    let ziumbiuli = Number(input[1])
+    let roses = Number(input[2])
+    let cactuses = Number(input[3])
+    let priceGift = Number(input[4])
+
+    let magnoliiPrice = 3.25
+    let ziumbiuliPrice = 4
+    let rosesPrice = 3.50
+    let cactusesPrice = 8
+
+    let totalPrice = (magnolii * magnoliiPrice + ziumbiuli * ziumbiuliPrice + roses * rosesPrice + cactuses * cactusesPrice) * 0.95
+    let difference = Math.abs(totalPrice - priceGift)
+
+    if (totalPrice >= priceGift){
+        console.log(`She is left with ${Math.floor(difference)} leva.`)
+    } else {
+        console.log(`She will have to borrow ${Math.ceil(difference)} leva.`)
+    }
+}
+
+flowerShop(["2", "3", "5", "1", "50"])
+flowerShop(["15", "7", "5", "10", "100"])
