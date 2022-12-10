50d7661e12eb705a9750b874f656ec1a4d5b72c2
Hristina Mateeva <hmateeva@gmail.com>
2022-12-11 00:14:44 +0200

    Create 05_smallShop.js

---
+++ 05_JavaScriptBasics/01_Lectures/03_conditionalStatementsAdvanced/05_smallShop.js
@@ -0,0 +1,67 @@
+function smallShop(input){
+    let product = input[0];
+    let town = input[1];
+    let quantity = Number(input[2]);
+
+    switch (town){
+        case "Sofia":
+            switch (product){
+                case "coffee":
+                    console.log(quantity * 0.50);
+                    break;
+                case "water":
+                    console.log(quantity * 0.80)
+                    break;
+                case "beer":
+                    console.log(quantity * 1.20)
+                    break;
+                case "sweets":
+                    console.log(quantity * 1.45)
+                    break;
+                case "peanuts":
+                    console.log(quantity * 1.60)
+            }
+            break;
+        case "Plovdiv":
+            switch (product){
+                case "coffee":
+                    console.log(quantity * 0.40);
+                    break;
+                case "water":
+                    console.log(quantity * 0.70)
+                    break;
+                case "beer":
+                    console.log(quantity * 1.15)
+                    break;
+                case "sweets":
+                    console.log(quantity * 1.30)
+                    break;
+                case "peanuts":
+                    console.log(quantity * 1.50)
+            }
+            break;
+        case "Varna":
+            switch (product){
+                case "coffee":
+                    console.log(quantity * 0.45);
+                    break;
+                case "water":
+                    console.log(quantity * 0.70)
+                    break;
+                case "beer":
+                    console.log(quantity * 1.10)
+                    break;
+                case "sweets":
+                    console.log(quantity * 1.35)
+                    break;
+                case "peanuts":
+                    console.log(quantity * 1.55)
+            }
+            break;
+    }
+}
+
+smallShop(["coffee", "Varna", "2"])
+smallShop(["peanuts", "Plovdiv", "1"])
+smallShop(["beer", "Sofia", "2"])
+smallShop(["sweets", "Sofia", "2.23"])
