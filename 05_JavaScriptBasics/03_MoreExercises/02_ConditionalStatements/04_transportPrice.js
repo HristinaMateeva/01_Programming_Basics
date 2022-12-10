f8d3725fbe478a15a22620336f89064f5827ad49
Hristina Mateeva <hmateeva@gmail.com>
2022-12-10 20:23:21 +0200

    Create 04_transportPrice.js

---
+++ 05_JavaScriptBasics/03_MoreExercises/02_ConditionalStatements/04_transportPrice.js
@@ -0,0 +1,34 @@
+function transportPrice(input){
+    let kilometers = Number(input[0])
+    let daytime = input[1]
+    let priceDayTaxi = 0.79
+    let priceNightTaxi = 0.90
+    let priceBus = 0.09 // minumum 20km
+    let priceTrain = 0.06 // minimum 100 km
+
+    let totalPrice = 0
+
+    if (daytime === "day"){
+        if (kilometers < 20){
+            totalPrice = 0.70 + kilometers * priceDayTaxi
+        } else if (kilometers >= 20 && kilometers < 100){
+            totalPrice = kilometers * priceBus
+        } else if (kilometers >= 100){
+            totalPrice = kilometers * priceTrain
+        }
+    } else if (daytime === "night"){
+        if (kilometers < 20){
+            totalPrice = 0.70 + kilometers * priceNightTaxi
+        } else if (kilometers >= 20 && kilometers < 100){
+            totalPrice = kilometers * priceBus
+        } else if (kilometers >= 100){
+            totalPrice = kilometers * priceTrain
+        }
+    }
+    console.log(`${totalPrice.toFixed(2)}`)
+}
+
+transportPrice(["5", "day"])
+transportPrice(["7", "night"])
+transportPrice(["25", "day"])
+transportPrice(["180", "night"])
