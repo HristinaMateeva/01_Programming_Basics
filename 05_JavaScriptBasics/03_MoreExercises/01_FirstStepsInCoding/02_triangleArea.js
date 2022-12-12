73ca315e04f8115078a50a1d14e16b7d10187451
Hristina Mateeva <hmateeva@gmail.com>
2022-12-12 19:22:13 +0200

    Update 02_triangleArea.js

--- 05_JavaScriptBasics/03_MoreExercises/01_FirstStepsInCoding/02_triangleArea.js
+++ 05_JavaScriptBasics/03_MoreExercises/01_FirstStepsInCoding/02_triangleArea.js
@@ -5,8 +5,3 @@ function triangleArea(input){
 
     console.log(area.toFixed(2))
 }
-
-triangleArea(["20", "30"])
-triangleArea(["15", "35"])
-triangleArea(["7.75", "8.45"])
-triangleArea(["1.23456", "4.56789"])
