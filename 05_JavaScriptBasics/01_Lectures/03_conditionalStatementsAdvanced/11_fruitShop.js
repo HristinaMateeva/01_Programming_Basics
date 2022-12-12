29425a422019c94a3f9daa31d71fbdb81a3a96a3
Hristina Mateeva <hmateeva@gmail.com>
2022-12-12 19:12:33 +0200

    Update 11_fruitShop.js

--- 05_JavaScriptBasics/01_Lectures/03_conditionalStatementsAdvanced/11_fruitShop.js
+++ 05_JavaScriptBasics/01_Lectures/03_conditionalStatementsAdvanced/11_fruitShop.js
@@ -84,9 +84,3 @@ function fruitShop(input){
             break;
     }
 }
-
-fruitShop(["apple", "Tuesday", "2"]);
-fruitShop(["orange", "Sunday", "3"]);
-fruitShop(["kiwi", "Monday", "2.5"]);
-fruitShop(["grapes", "Saturday", "0.5"]);
-fruitShop(["tomato", "Monday", "0.5"]);
