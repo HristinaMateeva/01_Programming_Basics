91943e48e32808b92501edf45258fe82b3c0a45e
Hristina Mateeva <hmateeva@gmail.com>
2022-12-12 19:12:18 +0200

    Update 10_invalidNumber.js

--- 05_JavaScriptBasics/01_Lectures/03_conditionalStatementsAdvanced/10_invalidNumber.js
+++ 05_JavaScriptBasics/01_Lectures/03_conditionalStatementsAdvanced/10_invalidNumber.js
@@ -5,13 +5,3 @@ function invalidNumber(input){
         console.log("invalid");
     }
 }
-
-
-invalidNumber(["75"]);
-invalidNumber(["150"]);
-invalidNumber(["220"]);
-invalidNumber(["199"]);
-invalidNumber(["-1"]);
-invalidNumber(["100"]);
-invalidNumber(["200"]);
-invalidNumber(["0"]);
