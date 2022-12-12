d82afa600dd3acdd3efb79189ac0f73797401d42
Hristina Mateeva <hmateeva@gmail.com>
2022-12-12 19:24:06 +0200

    Update 07_housePainting.js

--- 05_JavaScriptBasics/03_MoreExercises/01_FirstStepsInCoding/07_housePainting.js
+++ 05_JavaScriptBasics/03_MoreExercises/01_FirstStepsInCoding/07_housePainting.js
@@ -16,6 +16,3 @@ function housePainting(input){
     console.log(totalPaintWalls.toFixed(2))
     console.log(totalPaintRoof.toFixed(2))
 }
-
-housePainting(["6", "10", "5.2"])
-housePainting(["10.25", "15.45", "8.88"])
