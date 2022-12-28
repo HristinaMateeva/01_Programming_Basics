aa62eb62f86417bcbd3fccbf9eb050623013951f
Hristina Mateeva <hmateeva@gmail.com>
2022-12-28 19:53:51 +0200

    Create 01_readText.js

---
+++ 05_JavaScriptBasics/01_Lectures/05_whileLoop/01_readText.js
@@ -0,0 +1,14 @@
+function readText(input){
+    let index = 0;
+    let word = input[index];
+    index++;
+
+    while (word !== "Stop"){
+        console.log(word);
+        word = input[index];
+        index++;
+    }
+}
+
+readText(["Nakov", "SoftUni", "Sofia", "Bulgaria", "SomeText", "Stop", "AfterStop", "Europe", "HelloWorld"]);
+readText(["Sofia", "Berlin", "Moscow", "Athens", "Madrid", "London", "Paris", "Stop", "AfterStop"]);
