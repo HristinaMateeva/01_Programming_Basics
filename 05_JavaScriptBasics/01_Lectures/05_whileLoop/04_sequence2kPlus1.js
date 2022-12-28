facb765bff12402f3f10ee01cca430e428f9b0b5
Hristina Mateeva <hmateeva@gmail.com>
2022-12-28 19:53:58 +0200

    Create 04_sequence2kPlus1.js

---
+++ 05_JavaScriptBasics/01_Lectures/05_whileLoop/04_sequence2kPlus1.js
@@ -0,0 +1,14 @@
+function sequence2kPlus1(input) {
+    let number = Number(input[0]);
+    let k = 1;
+
+    while (k <= number){
+        console.log(k);
+        k = k * 2 + 1;
+    }
+}
+
+sequence2kPlus1(["3"]);
+sequence2kPlus1(["8"]);
+sequence2kPlus1(["17"]);
+sequence2kPlus1(["31"]);
