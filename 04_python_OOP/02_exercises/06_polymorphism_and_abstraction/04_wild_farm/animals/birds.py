2c08228c0bea16f8fe4e7333b936841cef3fc782
Hristina Mateeva <hmateeva@gmail.com>
2023-01-04 18:59:59 +0200

    Create birds.py

---
+++ 04_python_OOP/02_exercises/06_polymorphism_and_abstraction/04_wild_farm/animals/birds.py
@@ -0,0 +1,34 @@
+from animals.animal import Bird
+from food import Vegetable, Fruit, Meat, Seed
+
+
+class Owl(Bird):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Fruit, Meat, Seed]
+
+    @property
+    def gained_weight(self):
+        return 0.35
+
+    def make_sound(self):
+        return "Cluck"
