a4f2b953bb0713a69db654ca3c8e6c48d9828f78
Hristina Mateeva <hmateeva@gmail.com>
2023-01-10 19:47:26 +0200

    Create 02_even_parameters.py

---
+++ 04_python_OOP/02_exercises/09_decorators/02_even_parameters.py
@@ -0,0 +1,34 @@
+def even_parameters(function):
+    def wrapper(*args):
+        valid_data = [True if not isinstance(el, str) else False for el in args]
+        if not all(valid_data):
+            return "Please use only even numbers!"
+
+        result = [True if el % 2 == 0 else False for el in args]
+        if not all(result):
+            return "Please use only even numbers!"
+
+        return function(*args)
+    return wrapper
+
+
+# ------------------------------
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+
+
+# ------------------------------
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
