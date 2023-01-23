15321a43deaa23d96ae985b9756f06257404c4b4
Hristina Mateeva <hmateeva@gmail.com>
2023-01-23 18:32:06 +0200

    Create light_software.py

---
+++ 04_python_OOP/03_former_exams/10_exam-16.08.2020/project/software/light_software.py
@@ -0,0 +1,6 @@
+from project.software.software import Software
+
+
+# class LightSoftware(Software):
+#     def __init__(self):
+#         super().__init__(name="Hello", software_type="Light", capacity_consumption=)
