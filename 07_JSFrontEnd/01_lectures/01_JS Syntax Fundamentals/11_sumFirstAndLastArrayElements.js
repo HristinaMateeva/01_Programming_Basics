3f5cc894dc3b9d31c6bef7d618b4a847f4e05076
Hristina Mateeva <hmateeva@gmail.com>
2023-03-23 21:44:21 +0200

    Create 11_sumFirstAndLastArrayElements.js

---
+++ 07_JSFrontEnd/01_lectures/01_JS Syntax Fundamentals/11_sumFirstAndLastArrayElements.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastElement(numbers) {
+    let firstElement = numbers[0];
+    let lastElement = numbers[numbers.length-1];
+    console.log(firstElement + lastElement);
+}
+
+sumFirstAndLastElement([20, 30, 40]);
+sumFirstAndLastElement([10, 17, 22, 33]);
+sumFirstAndLastElement([11, 58, 69]);
