9e49d2f4f1137c95a489eb7b6398c48a8859ccaa
Hristina Mateeva <hmateeva@gmail.com>
2023-03-31 20:23:40 +0300

    Create 04_convertToObject.js

---
+++ 07_JSFrontEnd/01_lectures/03_Objects and Classes/04_convertToObject.js
@@ -0,0 +1,9 @@
+function convertToObj(jsonString) {
+    let data = JSON.parse(jsonString);
+    for (const key in data) {
+        console.log(`${key}: ${data[key]}`)
+    }
+}
+
+convertToObj('{"name": "George", "age": 40, "town": "Sofia"}');
+convertToObj('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
